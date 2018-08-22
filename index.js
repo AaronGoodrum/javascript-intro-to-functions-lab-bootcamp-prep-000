c10d03596b11a4f1febdf4be1a5908c373631daa
Aaron Goodrum <ag505181@gmail.com>
2018-08-22 15:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase === true) {
     return ('I can\'t hear you!')
-  } else {
-    if (string.toUpperCase === true){
-      return('YES INDEED!')
-    }
-  }
+}
+  // } else {
+  //   if (string.toUpperCase === true){
+  //     return('YES INDEED!')
+  //   }
+  // }
 }
